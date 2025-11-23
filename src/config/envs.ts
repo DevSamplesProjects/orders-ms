d12c596fc3e7b5453dc3078c7627e6e68ca4f808
DanMiramontes <dmiramontesg@gmial.com>
2025-11-23 00:49:21 -0600

    Integration nats - ms products

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,29 +1,32 @@
 import 'dotenv/config';
-
 import * as joi from 'joi';
 
 interface EnvVars {
   PORT: number;
   DATABASE_URL: string;
+  NATS_SERVERS: string[];
 }
 
-const envsSchema = joi.object({
+const envVarsSchema = joi.object({
   PORT: joi.number().required(),
   DATABASE_URL: joi.string().required(),
+  NATS_SERVERS: joi.array().items(joi.string()).required(),
 })
 .unknown(true);
 
-const { error, value } = envsSchema.validate( process.env );
+const { error, value } = envVarsSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(',')
+});
 
-
-if ( error ) {
-  throw new Error(`Config validation error: ${ error.message }`);
+if( error ) {
+  throw new Error(`Config validation error: ${error.message}`);
 }
 
-const envVars:EnvVars = value;
+const enVars: EnvVars = value;
 
-
-export const envs = {
-    PORT: envVars.PORT,
-    DATABASE_URL: envVars.DATABASE_URL,
-};
+export const  envs ={
+    PORT: enVars.PORT,
+    DATABASE_URL: enVars.DATABASE_URL,
+    NATS_SERVERS: enVars.NATS_SERVERS,
+}
